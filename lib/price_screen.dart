0fdd8cb02e99b91c7e63091b4f238fceb5dac470
mnaqeeb06 <muhammadnaqeebawan@gmail.com>
2022-08-07 12:03:43 -0700

    The Material DropdownButton Widget

--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -6,6 +6,7 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrecy = 'USD';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,7 +43,19 @@ class _PriceScreenState extends State<PriceScreen> {
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
             color: Colors.lightBlue,
-            child: null,
+            child: DropdownButton<String>(
+              value: selectedCurrecy,
+              items: [
+                DropdownMenuItem(child: Text('USD'), value: 'USD'),
+                DropdownMenuItem(child: Text('EUR'), value: 'EUR'),
+                DropdownMenuItem(child: Text('GPB'), value: 'GPB'),
+              ],
+              onChanged: (value) {
+                setState(() {
+                  selectedCurrecy = value!;
+                });
+              },
+            ),
           ),
         ],
       ),
